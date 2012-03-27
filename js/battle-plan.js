36d499505d533808ce91e57d70144f4d477a9d8c
Tim Collins <tim@tcollins.net>
2012-03-26 23:40:13 -0400

    sliding red square

--- js/battle-plan.js
+++ js/battle-plan.js
@@ -3,5 +3,22 @@ var bplan = {
 	init:		function(){
 					// Initialize Crafty
 					Crafty.init(640, 480);
+					
+					Crafty.scene("main",function() {
+					    Crafty.background("#ddd");
+					    bplan.player = Crafty.e("2D, DOM, Color, Slide, hero1")
+					         .color("red")
+					         .attr({x:30, y:90, w:50, h:50});
+					
+  					});
+  					  					
+  					
+  					Crafty.scene("main");
+					
+					
 				}
 }
+
+
+
+
